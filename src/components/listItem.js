8ba606ea50b5651e1217985d70f435233b9b5dc1
jChen-jc <gin85cc@gmail.com>
2022-06-09 20:19:43 +1000

    feat: render html content
    wrap with dompurify

--- src/components/listItem.js
+++ src/components/listItem.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import { Tooltip, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Tooltip } from '@material-ui/core'
+import DOMPurify from 'dompurify';
+import React from 'react';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     alignItems: 'flex-start',
@@ -16,35 +17,33 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     color: theme.palette.text.secondary,
   },
-}))
+}));
 /**
  * A component that renders icon and content in a defined way
  * @param {any} icon
- * @param {string} title 
+ * @param {string} title
  * @param {string} subtitle
- * @param {any} content  
+ * @param {any} content
  */
 
-const ListItem = ({
-  icon, 
-  title,
-  content,
-  tooltip,
-}) => {
+const ListItem = ({ icon, title, content, tooltip }) => {
   const classes = useStyles();
+  const contentHtml = DOMPurify.sanitize(content);
   return (
     <div className={classes.root}>
       <Tooltip title={tooltip}>
-        <div className={classes.icon}>
-          {icon}
-        </div>
+        <div className={classes.icon}>{icon}</div>
       </Tooltip>
       <div className={classes.content}>
-        {title && <Typography color="textSecondary" variant="subtitle2">{title}</Typography>}
-        {content}
+        {title && (
+          <Typography color='textSecondary' variant='subtitle2'>
+            {title}
+          </Typography>
+        )}
+        <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
       </div>
     </div>
-  )
+  );
 };
 
 export default ListItem;
